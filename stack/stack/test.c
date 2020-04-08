11609f5e58f7a744045bf1b983d0b499e4ec4489
Lastsummer <zhaopengfei_0106@126.com>
2020-04-08 10:04:29 +0800

    栈的实现

---
+++ stack/stack/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"stack.h"
+
+void test1(Stack* ps)
+{
+	StackPush(ps, 1);
+	StackPush(ps, 2);
+	StackPush(ps, 3);
+	StackPush(ps, 4);
+	StackPush(ps, 5);
+
+	StackPop(ps);
+	StackPop(ps);
+	StackPop(ps);
+	StackPop(ps);
+	StackPop(ps);
+	StackPop(ps);
+	StackPop(ps);
+
+	StackPush(ps, 5);
+	StackPush(ps, 4);
+	StackPush(ps, 3);
+	StackPush(ps, 2);
+	StackPush(ps, 1);
+
+
+}
+
+int main()
+{
+	Stack ps;
+	StackInit(&ps);
+	test1(&ps);
+	return 0;
+}
